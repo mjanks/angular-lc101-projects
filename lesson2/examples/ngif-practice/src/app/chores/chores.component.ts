78bcb53fe676e00e431966cd30dbcd41304cf0f2
mjanks <mjanks10@gmail.com>
2021-12-08 14:16:40 -0500

    *ngIf practice

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoresComponent implements OnInit {
    chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   finishedChores = ['Wash car', 'Cook dinner', 'Wash car', 'Cook dinner', 'Wash car', 'Cook dinner'];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://www.24hrpo.com/wp-content/uploads/2016/11/target-24HRPO.jpg';
 
    constructor() { }
 
